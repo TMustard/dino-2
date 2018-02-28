2a892bf51d056ca43562622f6f87f5dfff5ff08f
Tyler Mustard <tjm@Tylers-MacBook-Air.local>
2018-02-28 11:11:37 -0700

    ready

--- src/App.js
+++ src/App.js
@@ -4,19 +4,6 @@ import Footer from "./components/footer";
 import Profile from "./components/profile";
 import SkillList from "./components/skillList";
 
-// A deployed website
-// Built using a front end framework
-// All DOM manipulation is handled by front end framework
-// Makes a request to an api and renders a list with the results
-// Shows and hides skills list when the profile header is clicked.
-// Renders the skills array in a list for each profile
-// Has a component for the header
-// Has a component for the profiles
-// Has a component for the skill list
-// Has a component for the footer
-// Has the same look as the original page
-// All tests pass
-
 class App extends Component {
   state = { dinosaurs: [] };
 
@@ -34,7 +21,6 @@ class App extends Component {
 
   render() {
     const { dinosaurs } = this.state;
-    // console.log(dinosaurs);
     const profiles = dinosaurs.map((dinosaur, iterator) => {
       return (
         <Profile
@@ -45,9 +31,6 @@ class App extends Component {
         />
       );
     });
-    // const names = dinosaurs.map((dinosaur, iterator) => {
-    //   return;
-    // });
 
     return (
       <main>
@@ -63,34 +46,3 @@ class App extends Component {
 }
 
 export default App;
-
-// const itemOne = (
-//   <li>
-//     <div className="profile-card">
-//       <header className="profile-header">
-//         <img src="./assets/dino-1.png" />
-//         {profileHeaders[0]}
-//       </header>
-//       {profiles[0]}
-//     </div>
-//   </li>
-// );
-// const itemTwo = (
-//   <li>
-//     {profileHeaders[1]}
-//     {profiles[1]}
-//   </li>
-// );
-// const itemThree = (
-//   <li>
-//     {profileHeaders[2]}
-//     {profiles[2]}
-//   </li>
-// );
-// var combined = [];
-// for (var i = 0; i < 3; i++) {
-//   combined.push(profileHeaders[i]);
-//   // combined.push(profileHeaders[i].dinoPic);
-//   combined.push(profiles[i]);
-// }
-// console.log(combined);
